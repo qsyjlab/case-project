f5883c93eee640a925caa2096b6bbd12ab79185b
青衫依旧 <2461521018@qq.com>
2022-11-23 16:55:19 +0800

    feat: preview image

--- 图片预览/js/index.js
+++ 图片预览/js/index.js
@@ -1,75 +1,46 @@
-let modalInstance = null;
-
-let offset = {};
-
-// 中心点
-let origin = "center";
-let scale = 1;
-
-let startPoint = { x: 0, y: 0 }; // 记录初始触摸点位
-let isTouching = false; // 标记是否正在移动
-let isMove = false; // 正在移动中，与点击做区别
+import { updateStyle, adaptScale } from "./utils.js";
 
 export function init() {
   document.getElementById("list").addEventListener("click", (e) => {
     e.preventDefault();
 
     if (e.target.tagName === "IMG") {
-      const cloneTargetNode = e.target.cloneNode(true);
-
-      cloneTargetNode.style.opacity = 0;
-      preview(cloneTargetNode, e.target);
+      preview(e.target);
     }
   });
 }
 
-function preview(el, originEl) {
-  const mask = document.createElement("div");
+function preview(originEl) {
+  const el = originEl.cloneNode(true);
 
-  modalInstance = mask;
-  mask.classList.add("modal");
+  el.style.opacity = 0;
 
-  document.body.appendChild(mask);
+  let offset = {};
+
+  // 中心点
+  let origin = "center";
+  let scale = 1;
+  let lastScale = 1;
+  let lastDistance = 0;
+
+  let scaleOrigin = { x: 0, y: 0 };
+
+  let startPoint = { x: 0, y: 0 }; // 记录初始触摸点位
+  let isTouching = false; // 标记是否正在移动
+  let isMove = false; // 正在移动中，与点击做区别
+
+  let touches = new Map();
+
+  const maskInstance = createMask();
+
+  document.body.appendChild(maskInstance);
 
   const { top, left } = originEl.getBoundingClientRect();
   el.style.opacity = 1;
 
-  function maskClickFn(e, mask) {
-    if (e.stopPropagation) {
-      e.stopPropagation(); //非IE
-    } else {
-      e.cancelBubble = true; //兼容IE
-    }
-    // console.log('close');
+  updateStyle(el, [`left: ${left}px`, `top: ${top}px`]);
 
-    if (isMove) {
-      isMove = false;
-    } else {
-      changeStyle(el, [
-        "transition: all .3s",
-        `left: ${left}px`,
-        `top: ${top}px`,
-        `transform: translate(0,0)`,
-        `width: ${originEl.offsetWidth}px`,
-      ]);
-      setTimeout(() => {
-        document.body.removeChild(mask);
-        originEl.style.opacity = 1;
-        mask.removeEventListener("click", maskClickFn);
-      }, 300);
-    }
-  }
-
-  mask.addEventListener("click", (e) => maskClickFn(e, mask));
-
-  // 事件
-  mask.addEventListener("mousewheel", (e) => zoom(e, el, originEl), {
-    passive: false,
-  });
-
-  changeStyle(el, [`left: ${left}px`, `top: ${top}px`]);
-
-  mask.appendChild(el);
+  maskInstance.appendChild(el);
 
   const { innerWidth, innerHeight } = window;
 
@@ -77,7 +48,7 @@ function preview(el, originEl) {
     x: originEl.offsetWidth / 2 + left,
     y: originEl.offsetHeight / 2 + top,
   };
-
+  // 中心点位置
   const winCenterPoint = { x: innerWidth / 2, y: innerHeight / 2 };
 
   const offsetDistance = {
@@ -86,56 +57,20 @@ function preview(el, originEl) {
   };
 
   const diffs = {
-    left: ((adaptScale(originEl) - 1) * originEl.offsetWidth) / 2,
-    top: ((adaptScale(originEl) - 1) * originEl.offsetHeight) / 2,
+    left: ((getOriginElScale() - 1) * originEl.offsetWidth) / 2,
+    top: ((getOriginElScale() - 1) * originEl.offsetHeight) / 2,
   };
 
-  changeStyle(el, [
+  updateStyle(el, [
     "transition: all 0.3s",
-    `width: ${originEl.offsetWidth * adaptScale(originEl) + "px"}`,
+    `width: ${originEl.offsetWidth * getOriginElScale() + "px"}`,
     `transform: translate(${offsetDistance.left - left - diffs.left}px, ${
       offsetDistance.top - top - diffs.top
     }px)`,
   ]);
 
-  // 鼠标/手指按下
-  window.addEventListener("pointerdown", (e) => {
-    e.preventDefault();
-    isTouching = true;
-    startPoint = { x: e.clientX, y: e.clientY };
-  });
-  // 鼠标/手指抬起
-  window.addEventListener("pointerup", (e) => {
-    e.preventDefault();
-    isTouching = false;
-    setTimeout(() => {
-      isMove = false;
-    }, 300);
-  });
-  // 鼠标/手指移动
-  window.addEventListener("pointermove", (e) => {
-    if (isTouching) {
-      isMove = true;
-      // 单指滑动/鼠标移动
-      offset = {
-        left: offset.left + (e.clientX - startPoint.x),
-        top: offset.top + (e.clientY - startPoint.y),
-      };
-      changeStyle(el, [
-        'transition: all 0s',
-        `transform: translate(${offset.left + "px"}, ${
-          offset.top + "px"
-        }) scale(${scale})`,
-        `transform-origin: ${origin}`,
-      ]);
-      // 注意移动完也要更新初始点位，否则图片会加速逃逸可视区域
-      startPoint = { x: e.clientX, y: e.clientY };
-    }
-  });
-
-  //   // 动画结束后消除定位重置的偏差
   setTimeout(() => {
-    changeStyle(el, [
+    updateStyle(el, [
       "transition: all 0s",
       `left: 0`,
       `top: 0`,
@@ -148,45 +83,180 @@ function preview(el, originEl) {
       top: offsetDistance.top - diffs.top,
     }; // 记录值
   }, 300);
-}
 
-// 滚轮缩放
-const zoom = (event, el) => {
-  if (!event.deltaY) {
-    return;
+  bindEventHandler();
+
+  function bindEventHandler() {
+    maskInstance.addEventListener("click", (e) =>
+      maskClickEventHandler(e, maskInstance)
+    );
+    maskInstance.addEventListener(
+      "mousewheel",
+      (e) => zoomEventHandler(e, el),
+      {
+        passive: false,
+      }
+    );
   }
-  event.preventDefault();
-  origin = `${event.offsetX}px ${event.offsetY}px`;
 
-  if (event.deltaY < 0) {
-    scale += 0.2; // 放大
-  } else if (event.deltaY > 0) {
-    scale >= 0.3 && (scale -= 0.2); // 缩小
+  // 模态框点击事件
+  function maskClickEventHandler(e, maskInstance) {
+    if (isMove) {
+      isMove = false;
+    } else {
+      updateStyle(el, [
+        "transition: all .3s",
+        `left: ${left}px`,
+        `top: ${top}px`,
+        `transform: translate(0,0)`,
+        `width: ${originEl.offsetWidth}px`,
+      ]);
+      initilizeVal();
+      setTimeout(() => {
+        document.body.removeChild(maskInstance);
+        originEl.style.opacity = 1;
+        maskInstance.removeEventListener("click", maskClickEventHandler);
+      }, 300);
+    }
   }
-  changeStyle(el, [
-    "transition: all .15s",
-    `transform-origin: ${origin}`,
-    `transform: translate(${offset.left + "px"}, ${
-      offset.top + "px"
-    }) scale(${scale})`,
-  ]);
-};
 
-// 用于修改样式的工具类，并且可以减少回流重绘，后面代码中会频繁用到
-function changeStyle(el, arr) {
-  const original = el.style.cssText.split(";");
-  original.pop();
-  el.style.cssText = original.concat(arr).join(";") + ";";
-}
-
-// 动态计算 缩放倍率
-function adaptScale(el) {
-  const { innerWidth, innerHeight } = window;
-  const { offsetWidth: w, offsetHeight: h } = el; // 获取文档中图片的宽高
-  let scale = 0;
-  scale = innerWidth / w;
-  if (h * scale > innerHeight - 80) {
-    scale = (innerHeight - 80) / h;
+  function getOriginElScale() {
+    return adaptScale(originEl);
   }
-  return scale;
+
+  // 鼠标/手指按下
+  window.addEventListener("pointerdown", (e) => {
+    e.preventDefault();
+    isTouching = true;
+
+    touches.set(e.pointerId, e);
+
+    if (touches.size === 2) {
+      lastDistance = getDistance();
+      lastScale = scale;
+    }
+
+    startPoint = { x: e.clientX, y: e.clientY };
+  });
+  // 鼠标/手指抬起
+  window.addEventListener("pointerup", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    isTouching = false;
+
+    setTimeout(() => {
+      isMove = false;
+    }, 0);
+    touches.delete(e.pointerId);
+  });
+  // 鼠标/手指移动
+  window.addEventListener("pointermove", (e) => {
+    if (!isTouching) return false;
+
+    isMove = true;
+
+    if (touches.size < 2) {
+      // 单指滑动/鼠标移动
+      offset = {
+        left: offset.left + (e.clientX - startPoint.x),
+        top: offset.top + (e.clientY - startPoint.y),
+      };
+      updateStyle(el, [
+        "transition: all 0s",
+        `transform: translate(${offset.left + "px"}, ${
+          offset.top + "px"
+        }) scale(${scale})`,
+        `transform-origin: ${origin}`,
+      ]);
+      // 注意移动完也要更新初始点位，否则图片会加速逃逸可视区域
+      startPoint = { x: e.clientX, y: e.clientY };
+    } else {
+      touches.set(e.pointerId, e); // 更新点位数据
+      const ratio = getDistance() / lastDistance; // 比较距离得出比例
+      scale = ratio * lastScale;
+      offset = getOffsetCorrection();
+      updateStyle(el, [
+        "transition: all 0s",
+        `transform: translate(${offset.left + "px"}, ${
+          offset.top + "px"
+        }) scale(${scale})`,
+        `transform-origin: ${origin}`,
+      ]);
+    }
+  });
+  // 滚轮缩放
+  function zoomEventHandler(event, el) {
+    if (!event.deltaY) {
+      return;
+    }
+    event.preventDefault();
+    origin = `${event.offsetX}px ${event.offsetY}px`;
+
+    if (event.deltaY < 0) {
+      scale += 0.2; // 放大
+    } else if (event.deltaY > 0) {
+      scale >= 0.3 && (scale -= 0.2); // 缩小
+    }
+
+    offset = getOffsetCorrection(event.offsetX, event.offsetY);
+    updateStyle(el, [
+      "transition: all .15s",
+      `transform-origin: ${origin}`,
+      `transform: translate(${offset.left + "px"}, ${
+        offset.top + "px"
+      }) scale(${scale})`,
+    ]);
+  }
+
+  function createMask() {
+    const maskInstance = document.createElement("div");
+    maskInstance.classList.add("modal");
+
+    return maskInstance;
+  }
+
+  // 初始化值
+  function initilizeVal() {
+    offset = {};
+
+    // 中心点
+    origin = "center";
+    scale = 1;
+    lastScale = 1;
+    lastDistance = 0;
+
+    scaleOrigin = { x: 0, y: 0 };
+
+    startPoint = { x: 0, y: 0 }; // 记录初始触摸点位
+    isTouching = false; // 标记是否正在移动
+    isMove = false; // 正在移动中，与点击做区别
+
+    touches = new Map();
+  }
+
+  function getDistance() {
+    const touchArr = Array.from(touches);
+    if (touchArr.length < 2) {
+      return 0;
+    }
+    const start = touchArr[0][1];
+    const end = touchArr[1][1];
+    return Math.hypot(end.x - start.x, end.y - start.y);
+  }
+
+  // 获取中心改变的偏差
+  function getOffsetCorrection(x = 0, y = 0) {
+    const touchArr = Array.from(touches);
+    if (touchArr.length === 2) {
+      const start = touchArr[0][1];
+      const end = touchArr[1][1];
+      x = (start.offsetX + end.offsetX) / 2;
+      y = (start.offsetY + end.offsetY) / 2;
+    }
+    origin = `${x}px ${y}px`;
+    const offsetLeft = (scale - 1) * (x - scaleOrigin.x) + offset.left;
+    const offsetTop = (scale - 1) * (y - scaleOrigin.y) + offset.top;
+    scaleOrigin = { x, y };
+    return { left: offsetLeft, top: offsetTop };
+  }
 }
